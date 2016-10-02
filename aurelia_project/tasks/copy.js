d918f3795ca7602af6e0bf74c208b6c994513d6c
Kenneth Henderick <kenneth@ketronic.be>
2016-10-02 19:27:30 +0200

    Some CLI fiddling

--- aurelia_project/tasks/copy.js
+++ aurelia_project/tasks/copy.js
@@ -1,7 +1,17 @@
 import gulp from "gulp";
 import project from "../aurelia.json";
 
-export default function copy() {
-    return gulp.src(project.copy.src, project.copy.options)
-        .pipe(gulp.dest(project.copy.dest));
-};
+function copyModules() {
+    return gulp.src(project.copy.modules.src, project.copy.modules.options)
+        .pipe(gulp.dest(project.copy.modules.dest));
+}
+
+function copySrc() {
+    return gulp.src(project.copy.src.src, project.copy.src.options)
+        .pipe(gulp.dest(project.copy.src.dest))
+}
+
+export default gulp.series(
+    copyModules,
+    copySrc
+);
